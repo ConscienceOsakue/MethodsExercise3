67439d55372de5c33c96dacdf5b9ceed85d48936
conscience osakue <consciencenosa@gmail.com>
2023-11-22 14:56:38 -0600

    initial commit

--- MethodsExercise3/Program.cs
+++ MethodsExercise3/Program.cs
@@ -8,6 +8,27 @@ namespace MethodsExercise3
         static void Main(string[] args)
         {
 
+            //Write a method to check if an integer(from the user) is in the range -10 to 10.
+
+            var listOfNumber = new List<int>();
+            for (var num = -10; num <= 10; num++)
+
+            {
+                listOfNumber.Add(num);
+
+            }
+            Console.WriteLine("Type your number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (listOfNumber.Contains(userInput))
+            {
+                Console.WriteLine($"{userInput} is in range of -10 to 10.");
+            }
+            else
+            {
+                Console.WriteLine($"{userInput} is not found in the range of -10 to 10.");
+            }
+
         }
     }
 }
